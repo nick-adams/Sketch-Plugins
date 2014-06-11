a5aa4c2b438064fb6185150cd9f87d90ca5abd4f
Nick Adams <nick@nick-adams.com>
2014-06-11 15:43:22 -0400

    Initial Commit

---
+++ library/config.js
@@ -0,0 +1,25 @@
+configVars = {
+    
+    //Include a prefix? [yes/no]
+    includePrefix: "yes", 
+    
+	// By default, it will prompt you to enter a prefix (examples: launcher, menu, stat_notify, tab, dialog). 
+	// Un-comment the next line to set a prefix manually instead: 
+	// prefixName: "prefix",
+	
+	
+    // Do you want to include a suffix? [yes/no]
+    includeSuffix: "yes",
+    
+    // The default suffix is set to the page name (examples: normal, pressed, focused, disabled, selected). 
+    // Un-comment the next line to set a custom one manually instead:
+	// suffixName: "suffix",
+	
+	
+	// This plugin works best if you design @1x, but if you design @2x (or 3x, or 4x) you can change this number here:
+	baseSize: 1,
+	
+	// Include the asset width at the end of the file name? [yes/no]
+	size: "no"
+	
+}
